2e277744948c256d54e224ecc782e4c6f458b5ee
Jared Brown <jared.a.brown98@gmail.com>
2026-04-05 19:30:52 -0500

    build: update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   // add yur domain name here
-  site: 'https://lexingtonthemes.com',
+  site: 'https://thekidfrankie.com',
   compressHTML: true,
   integrations: [sitemap()]
 });
